df7f2b852be2533a3e449ccbc67f9c4336691045
Yaroslav <beyaroslav@gmail.com>
2021-05-15 18:24:27 +0700

    Homework 1 is done

---
+++ src/main/java/my/servlet/classes/Product.java
@@ -0,0 +1,13 @@
+package my.servlet.classes;
+
+public class Product {
+    public int id;
+    public String title;
+    public double cost;
+
+    public Product(int id, String title, double cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+}
